d6b07bc33ad553751ea838a58ca0360907ea470a
SUPER <superpung.lee@gmail.com>
2021-05-17 15:56:40 +0800

    feat(main): add display json str

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import json
 
 
 def crawl():
-    postUrl = 'http://pan.tju.edu.cn:9123/v1/link?method=get'
-    payloadData = {
+    # POST 01
+    post_url = 'http://pan.tju.edu.cn:9123/v1/link?method=get'
+    payload_data = {
         "link": "95A04E31C5BD079C95255EA95D16F10D",
         "password": "bdd5",
         "docid": ""
     }
-    payloadHeader = {
+    payload_header = {
         "Content-Type": "text/plain;charset=UTF-8",
         "Referer": "http://pan.tju.edu.cn/",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -17,20 +18,25 @@ def crawl():
     }
 
     s = requests.session()
-    r = s.post(postUrl, data=json.dumps(payloadData), headers=payloadHeader)
+    r = s.post(post_url, data=json.dumps(payload_data), headers=payload_header)
     print("POST Status code:", r.status_code)
-
-    print(r.text)
-    url = 'http://pan.tju.edu.cn:9123/v1/link?method=listdir'
-    data = {
+    display_json(r.text)
+    # POST 02
+    content_url = 'http://pan.tju.edu.cn:9123/v1/link?method=listdir'
+    content_data = {
         "link": "95A04E31C5BD079C95255EA95D16F10D",
         "password": "bdd5",
         "by": "name",
         "sort": "asc"
     }
-    getUrl = s.post(url, data=json.dumps(data), headers=payloadHeader)
-    text = getUrl.text
-    print(json.loads(text))
+    get_url = s.post(content_url, data=json.dumps(content_data), headers=payload_header)
+    display_json(get_url.text)
+
+
+def display_json(json_str):
+    result = json.loads(json_str)
+    for key in result:
+        print(key, ":", result[key])
 
 
 if __name__ == '__main__':
