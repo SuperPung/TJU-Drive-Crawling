1e408335e7ca96edfb32d68efd5ebb269b5ca02a
SUPER <superpung.lee@gmail.com>
2021-05-17 15:35:18 +0800

    feat(main): first request

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def crawl():
+    postUrl = 'http://pan.tju.edu.cn:9123/v1/link?method=get'
+    payloadData = {
+        "link": "95A04E31C5BD079C95255EA95D16F10D",
+        "password": "bdd5",
+        "docid": ""
+    }
+    payloadHeader = {
+        "Content-Type": "text/plain;charset=UTF-8",
+        "Referer": "http://pan.tju.edu.cn/",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/90.0.4430.212 Safari/537.36 "
+    }
+
+    s = requests.session()
+    r = s.post(postUrl, data=json.dumps(payloadData), headers=payloadHeader)
+    print("POST Status code:", r.status_code)
+
+    print(r.text)
+    url = 'http://pan.tju.edu.cn:9123/v1/link?method=listdir'
+    data = {
+        "link": "95A04E31C5BD079C95255EA95D16F10D",
+        "password": "bdd5",
+        "by": "name",
+        "sort": "asc"
+    }
+    getUrl = s.post(url, data=json.dumps(data), headers=payloadHeader)
+    text = getUrl.text
+    print(json.loads(text))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    crawl()
